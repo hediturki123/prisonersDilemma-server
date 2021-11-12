a953e1842d08697bd5231b4272189ff3f4b1b9b8
Hedi Turki <hedi.turki.sanekli@gmail.com>
2021-11-12 10:59:57 +0100

    Ajout dépendance spring dans pom.xml, ajout du main RestServer et DbConnection

--- src/main/java/RestServer.java
+++ src/main/java/RestServer.java
@@ -1,4 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RestServer {
+    public static final boolean DEBUG=true;
+    public static void main(String[] args) {
+        SpringApplication.run(RestServer.class, args);
+    }
 
 }
--- src/main/java/utils/DbConnection.java
+++ src/main/java/utils/DbConnection.java
@@ -1,5 +1,37 @@
 package utils;
 
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DbConnection {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        System.out.println("dataSource !!!");
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            Properties pr = new Properties();
+            pr.setProperty("stringtype", "unspecified");
+            config.setJdbcUrl(dbUrl);
+            config.setDataSourceProperties(pr);
+            config.setMaximumPoolSize(3);
+            return new HikariDataSource(config);
+        }
+    }
 
 }
