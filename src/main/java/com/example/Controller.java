b09b6af74c0627503b505c60a9fafddf293f5bd8
Hedi Turki <hedi.turki.sanekli@gmail.com>
2021-11-17 21:37:24 +0100

    ajout readAllRounds

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -8,25 +8,27 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import object.Decision;
 import object.Game;
 import object.Player;
+import object.Round;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/")
+@RequestMapping("/home")
 public class Controller {
 	
-	@GetMapping("/home")
+	@GetMapping("/")
 	public ResponseEntity<Game> createGame() {
 		Player player1 = new Player();
 		return ResponseEntity.ok(player1.createGame(10));
 	}
 	
-	@GetMapping("/home/{idGame}")
+	@GetMapping("/{idGame}")
 	public ResponseEntity<Game> joinGame(@PathVariable(name = "idGame") int id) {
 		Player player2 = new Player();
 		Game game = findGameById(id);
@@ -34,7 +36,7 @@ public class Controller {
 		return ResponseEntity.ok(game);
 	}
 	
-	@GetMapping("/home/game/{id}")
+	@GetMapping("/game/{id}")
 	public ResponseEntity<Game> readGame(@PathVariable(name = "id") int id) {
 		return ResponseEntity.ok(findGameById(id));
 	}
@@ -64,14 +66,29 @@ public class Controller {
 		return RestServer.games.get(i);
 	}
 	
-	@GetMapping("/home/game/{idGame}/player/{idPlayer}")
+	@GetMapping("/game/{idGame}/player/{idPlayer}")
 	public ResponseEntity<Player> readPlayer(@PathVariable(name = "idGame") int idGame, 
 			@PathVariable(name = "idPlayer") int idPlayer) {
 		Game game = findGameById(idGame);
 		return ResponseEntity.ok(game.findPlayerById(idPlayer));			
 	}
 	
-	@GetMapping("home/game/{idGame}/allPlayers")
+	@PutMapping("/game/{idGame}/player/{idPlayer}")
+	public ResponseEntity<Player> updatePlayer(@PathVariable(name = "idGame") int idGame, 
+			@PathVariable(name = "idPlayer") int idPlayer) {
+		Game game = findGameById(idGame);
+		Player player = game.findPlayerById(idPlayer);
+		player.setScore(3);
+		return ResponseEntity.ok(player);			
+	}
+	
+	@GetMapping("/game/{idGame}/allRounds")
+	public ResponseEntity<List<Round>> readAllRounds(@PathVariable(name = "idGame") int idGame) {
+		Game game = findGameById(idGame);
+		return ResponseEntity.ok(game.getHistory());
+	}
+	
+	@GetMapping("/game/{idGame}/allPlayers")
 	public ResponseEntity<List<Player>> readAllPlayers(@PathVariable(name = "idGame") int idGame) {
 		Game game = findGameById(idGame);
 		return ResponseEntity.ok(game.allPlayers());
