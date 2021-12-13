84b18c7255d095c08624d162a6aeb2c05819ac5e
Pierre Bruyere <pierre.bruyere@fluoptics.com>
2021-12-13 10:54:33 +0100

    Add the 4 new strategies

---
+++ src/main/java/strategies/LunaticStrategy.java
@@ -0,0 +1,19 @@
+package strategies;
+
+import java.util.Random;
+import object.Decision;
+import object.Player;
+
+public class LunaticStrategy implements Strategy{
+
+	@Override
+	public void action(Player player) {
+		Random random = new Random();
+		boolean isDecisionCooperate = random.nextBoolean();
+		if (isDecisionCooperate) {
+			player.setCurrentDecision(Decision.COOPERATE);
+		} else {
+			player.setCurrentDecision(Decision.BETRAY);
+		}
+	}
+}
---
+++ src/main/java/strategies/PeriodicMean.java
@@ -0,0 +1,57 @@
+package strategies;
+
+import java.util.List;
+
+import com.example.RestServer;
+
+import object.Decision;
+import object.Game;
+import object.Player;
+import object.Round;
+
+public class PeriodicMean implements Strategy {
+
+	@Override
+	public void action(Player player) {
+    	int index = 0;
+    	if(RestServer.getGames().get(index).getPlayer2() != null)
+    	{
+    		while(index < RestServer.getGames().size() && RestServer.getGames().get(index).getPlayer1().getId() != player.getId() && RestServer.getGames().get(index).getPlayer2().getId() != player.getId()) {
+    			index++;
+    		}
+    	}else {
+    		while(index < RestServer.getGames().size() && RestServer.getGames().get(index).getPlayer1().getId() != player.getId()) {
+    			index++;
+    		}
+    	}
+		
+		if(index == RestServer.getGames().size()) {
+			player.setCurrentDecision(Decision.COOPERATE);
+		} else {
+			Game game = RestServer.getGames().get(index);
+			List<Round> rounds = game.getHistory();
+			if(rounds != null && rounds.size() > 1) {
+				Round lastRound = rounds.get(rounds.size() - 1);
+				Round antepenultimateRound = rounds.get(rounds.size() - 2);
+				if (player.getId() == game.getPlayer1().getId()) {
+					if(lastRound.getMovePlayer1() == Decision.BETRAY
+							&& antepenultimateRound.getMovePlayer1() == Decision.BETRAY) {
+						player.setCurrentDecision(Decision.COOPERATE);
+					}else {
+						player.setCurrentDecision(Decision.BETRAY);
+					}
+				}
+				if (player.getId() == game.getPlayer2().getId()) {
+					if(lastRound.getMovePlayer2() == Decision.BETRAY
+							&& antepenultimateRound.getMovePlayer2() == Decision.BETRAY) {
+						player.setCurrentDecision(Decision.COOPERATE);
+					}else {
+						player.setCurrentDecision(Decision.BETRAY);
+					}
+				}
+			}else {
+				player.setCurrentDecision(Decision.BETRAY);
+			}
+		}
+	}
+}
